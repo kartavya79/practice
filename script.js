50cafd9ef3aa15f5037938d4321c2f42f9ce5964
kartavya79 <kartavyagautam6@gmail.com>
2026-01-22 11:52:24 +0530

    second

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-const container=document.querySelector(".container")
-container.addEventListener("click",()=>{console.log("div")},true)
+// const container=document.querySelector(".container")
+// container.addEventListener("click",()=>{console.log("div")},true)
 
-const button=document.querySelector("button")
-button.addEventListener("click",()=>{console.log("button")},true)
+// const button=document.querySelector("button")
+// button.addEventListener("click",()=>{console.log("button")},true)
 
 
-const outer=document.querySelector(".outer")
-outer.addEventListener("click",()=>{console.log("outer div")},true)
+// const outer=document.querySelector(".outer")
+// outer.addEventListener("click",()=>{console.log("outer div")},true)
 
-console.log("new code")
+// console.log("new code")
+
+// console.log(b) show undefined
+// console.log(a) show error 
+
+
+let a=23534
+var b=5674
+
+console.log(a)
+console.log(b)
+
+function print(){
+    var c=40
+    console.log(c)
+    console.log("inside function")
+}
+
+print()
