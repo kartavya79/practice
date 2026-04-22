33433f51a250aee623738d167b817793e6a1894b
kartavya79 <kartavyagautam6@gmail.com>
2026-04-22 12:02:34 +0530

    feat: implement Contact component with form handling for user input

--- react/src/components/sample.jsx
+++ react/src/components/sample.jsx
@@ -1,89 +1,139 @@
-// import React from 'react'
-// import { add } from "./utility"
+// import React, { Component } from 'react'
 
-// function Sample(props) {
-//     console.log(add(2,4))
-//   return (
-//     <div>
-//       <h2>Trending Topics</h2>
-//       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quod voluptatum iure incidunt, temporibus sequi iusto, pariatur maxime commodi ducimus, nihil autem. Magni modi praesentium, fuga culpa ad animi laboriosam!</p>
-//        <button
-//           className="counter"
-//           onClick={() => props.setCount((count) => count + 1)}
-//         >
-//           Count is {props.count}
-//         </button>
-//     </div>
-//   )
+//  class Sample extends Component {
+//     constructor(props){
+//         super(props)
+//         this.state = {
+//             count: 0,
+//         }
+//     }
+
+//     componentDidMount(){
+//       console.log("Component is mounted")
+//     }
+
+//     shouldComponentUpdate(nextProps, nextState){
+//         // if(nextState.count === 1){
+//         //   return false
+//         // }
+//         // return true
+//         console.log("Component should update")
+//         return true
+//     }
+
+//     componentDidUpdate(){
+//       console.log("Component Did Update")
+//     }
+
+//     componentWillUnmount(){
+//       console.log("Component Will Unmount")
+//     }
+
+//     increment = () => {
+//         this.setState({count: this.state.count + 1})
+//     }
+//   render() {
+//     return (
+//       <div>
+//         <h1>This is a sample component</h1>
+//         <h2>{this.state.count}</h2>
+//         <button onClick={this.increment}>Increment</button>
+//       </div>
+//     )
+//   }
 // }
 
 // export default Sample
 
-import React, { useState } from 'react'
-// import style from "../css/sample.module.css"
-import styled from "@emotion/styled";
-import { css } from "@emotion/react";
 
+import React from 'react'
+import { useState, useEffect, useRef } from 'react'
+import styles from "../css/Sample.module.css"
+import NavStyle from "../css/NavBar.module.css"
+// import styled from "styled-components"
+import styled from "@emotion/styled"
+import { css } from '@emotion/react'
+import axios from "axios"
 
 function Sample() {
-  const [count, setCount] = useState(0)
-  const [name , setName] = useState("kartavya")
+  // let count=0
+  const [count, setCount]=useState(0)
+  const[name, setName]= useState("Devendra")
+  const [loading, setLoading]=useState(false)
+  const inputRef = useRef(null)
 
+  useEffect(()=>{
+    console.log("Component is mounted")
+    inputRef.current.focus()
+    return () => {
+      console.log("Component unmount")
+    }
+  },[name])
+
+  useEffect(()=>{
+    // const fetchData = async() => {
+    //   try{
+    //     const response = await fetch("https://jsonplaceholder.typicode.com/todos/500",{
+    //       method: "GET"
+    //     })
+    //     const json = await response.json()
+    //     console.log(json)
+    //   }catch(e){
+    //     console.log(e)
+    //   }
+    // }
+
+    const fetchData = async() => {
+      try{
+        setLoading(true)
+        const response = await axios.get("https://jsonplaceholder.typicode.com/todos/5")
+        console.log(response.data)
+        setLoading(false)
+      }catch(e){
+        
+          console.log(e)
+      }
+    }
+
+    fetchData()
+  },[])
+  
+  const increment = () => {
+       setCount(count + 1)
+    }
+
+    const decrement = () => {
+       setCount(count - 1)
+    }
   return (
     <div>
-      <h1>This is Sample Component</h1>
+      <h1>This is a sample component</h1>
       <h2>{count}</h2>
       <h3>{name}</h3>
-      <button onClick={()=>setName("krish")}>Change Name</button>
-
-      <button css={functionbutton} onClick={() => setCount(count + 1)}>
-        Increment
-      </button>
-      {/* <button className={style.btn} onClick={() => setCount(count + 1)}>
-        Increment
-      </button>
-       */}
-
-      <button style={{ backgroundColor: "yellow", color: "black", width: "100px", height: "30px" }} onClick={() => setCount(count - 1)}>
-        Decrement
-      </button>
-      <button className='bg-red-600 p-0.5 text-white'>sampled</button>
+      {loading?<h2>Loading...</h2>:""}
+      <input type='text' ref={inputRef} placeholder='Enter the value' />
+      <button onClick={()=>{inputRef.current.value="Alex"}}>Fill the text</button>
+      <button onClick={()=>setName("Alex")}>Change the Name</button>
+      <button css={funcButton} onClick={increment}>Increment</button>
+      <Button danger onClick={decrement}>decrement</Button>
+      <button className='bg-blue-600 text-white w-18'>Sample </button>
     </div>
   )
 }
-const functionbutton=css`
-  background-color: blue;
-  color: white;
-  width: 100px;
-  height: 30px;
-`
-const Button = styled.button`
-  background-color: ${(props) => props.danger ? "red" : "lightgray"};
-  color: ${(props) => props.color || "black"};
-  width: 100px;
-  height: 30px;
-`
 
 export default Sample
 
-// function Sample(){
-//     const[count,setCount] = useState(0)
-
-//     const increment = () => {
-//         setCount(count + 1)
-//     }
-//     const decrement = () => {
-//         setCount(count - 1)
-//     }
-//     return(
-//         <div>
-//             <h1>This is a sample component</h1>
-//             <h2>{count}</h2>
-//             <button onClick={increment}>Increment</button>
-//             <button onClick={decrement}>Decrement</button>
-//         </div>
-//     )
-// }
-
-
-
+const funcButton=css`
+    background-color: yellow;
+    color: white;
+    width: 100px;
+    height: 30px;
+    border-radius: 10px;
+  ` 
+const Button=styled.button`
+    background-color: ${props=>props.danger ? "red" : "green"};
+    color: white;
+    width: 100px;
+    height: 30px;
+    border-radius: 10px;
+  `
